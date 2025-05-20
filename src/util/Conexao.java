1eed2115766378e1cac5ec6b6bc25a5c7368dff3
Gabriel lindu <gabriel.bsmaldonado@senacsp.edu.br>
2025-05-20 17:18:50 -0300

    Conexão reconectada

--- src/util/Conexao.java
+++ src/util/Conexao.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class Conexao {
 
-    private String driver = "com.mysql.jdbc.Driver";
+    private String driver = "com.mysql.cj.jdbc.Driver";
     private String url = "jdbc:mysql://127.0.0.1:3306/app_hotelaria";
-    private String usuario = "app_hotelaria";
+    private String usuario = "dbaHotel";
     private String senha = "dbaHotel123";
 
 
@@ -20,17 +20,14 @@ public class Conexao {
             // Especifica a rota do driver a ser carregado (JDBC para SGBD MySQL)
             Class.forName(driver);
 
-
         /* Iniciar o driver já carregado por meio
         do metodo getConnection(IP, porta, nome do banco, usuario, senha)*/
-            DriverManager.getConnection(url, usuario, senha);
+            condb = DriverManager.getConnection(url, usuario, senha);
+            return condb;
 
-
-        } catch (SQLException erro) {
+        } catch (SQLException | ClassNotFoundException erro) {
             System.out.println("Erro ao conectar ao Banco de Dados: " + erro);
             return null;
         }
-
-
     }
 }
